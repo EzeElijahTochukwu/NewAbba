2400850cfb32c01376e55eff9f4dbc51f59312ed
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-04 10:58:20 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,14 @@ const Footer = () => {
           <h3>Contact Us</h3>
           <p><a href="tel:08127945747"><i className="fas fa-phone"></i> 08127945747</a></p>
           <p><a href="tel:07031179735"><i className="fas fa-phone"></i> 07031179735</a></p>
-          <p><a href="mailto:info@yesabbawrites.online"><i className="fas fa-envelope"></i> info@yesabbawrites.online</a></p>
+          <p><a href="https://www.instagram.com/yesabbawrites?igsh=MTc5c2d1ZWtwNjB3Zw=="><i className="fas fa-instagram"></i> yesabbawrites</a></p>
           <p><a href="https://ng.linkedin.com/company/yesabba-writes?trk=public_post_feed-actor-name"><i className="fab fa-linkedin"></i> YesAbba Writes</a></p>
+          <p><a href="mailto:info@yesabbawrites.online"><i className="fas fa-envelope"></i> info@yesabbawrites.online</a></p>
         </div>
 
         {/* Right Section - Navigation Links */}
         <div className="footer-right">
+          <h3>Pages</h3>
           <div className="footer-links">
             <a href="/">Home</a>
             <a href="#services">Services</a>
