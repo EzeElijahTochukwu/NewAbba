4cc1926010c920663d47befef8fcd31c66c3337e
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-04 12:54:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from 'react'
 import Header from "./components/Header";
 import Services from "./components/Services";
 import Blog from "./pages/Blog";
-import Contact from "./pages/Contact";
-import AboutUs from "./pages/AboutUs";
-import Testimonial from "./pages/Testimonial";
 import Footer from "./components/Footer";
 import "./index.css";
-import "./App.css";
+import './App.css'
 
 const App = () => {
   return (
-    <Router>
+    <div>
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<Services />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/testimonials" element={<Testimonial />} />
-        </Routes>
+        <Services />
+        
       </main>
       <Footer />
-    </Router>
+    </div>
   );
 };
 
-export default App;
+
+
+export default App
