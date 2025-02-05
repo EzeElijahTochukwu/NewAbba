b2fe954aec47db6452c7067755cab6f9dcd417a8
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 01:59:25 +0100

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,1 +1,12 @@
+// src/pages/Services.js
+import React from 'react';
+
+const Services = () => (
+  <div>
+    <h2>Our Services</h2>
+    <p>Details about our services.</p>
+  </div>
+);
+
+export default Services;
 
