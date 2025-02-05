886ad64a52e245d11cb09836c58b3c5a8e1a19a6
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:12:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Services from "./components/Services"; // Our Services component
-import Blog from "./pages/Blog";
-
-// Importing the additional pages
-import Home from "./pages/Home";
-import Testimonials from "./pages/Testimonials";
-import Contact from "./pages/Contact";
-
+import Services from "./components/Services"; // You can choose to show a summary of services on the home page
 import "./index.css";
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/testimonials" element={<Testimonials />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <main>
+        {/* You can include additional content for your home page here */}
+        <Services />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
