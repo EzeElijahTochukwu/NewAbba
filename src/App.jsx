3bef6f8c685cd6abf2b4333a27c272304c616284
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:05:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
-import { useState } from 'react'
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Header from "./components/Header";
-import Services from "./components/Services";
-import Blog from "./pages/Blog";
 import Footer from "./components/Footer";
+import Services from "./components/Services"; // Our Services component
+import Blog from "./pages/Blog";
+
+// Importing the additional pages
+import Home from "./pages/Home";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+
 import "./index.css";
-import './App.css'
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <main>
-        <Services />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
-
-
-export default App
+export default App;
