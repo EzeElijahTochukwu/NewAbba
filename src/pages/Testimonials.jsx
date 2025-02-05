10d55632d3ed5b6445dd4b7bbfa96b389067bf90
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:00:05 +0100

    Create Testimonials.jsx

---
+++ src/pages/Testimonials.jsx
@@ -0,0 +1,11 @@
+// src/pages/Testimonials.js
+import React from 'react';
+
+const Testimonials = () => (
+  <div>
+    <h2>Testimonials</h2>
+    <p>Read what our clients have to say.</p>
+  </div>
+);
+
+export default Testimonials;
