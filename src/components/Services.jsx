5489621d2f33cea5069c96a26ac96b7380bb3c30
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 01:38:25 +0100

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,37 +8,37 @@ const Services = () => {
         <div className="service">
           <h3>Content Writing</h3>
           <p>
-            We refine our content creation process to produce polished, professional pieces that reflect our collective vision.
+            We refine our content creation process to produce polished, professional pieces that reflect collective vision.
           </p>
         </div>
         <div className="service">
           <h3>Copywriting</h3>
           <p>
-            We craft compelling copy that fuels engagement and drives our shared success.
+            We craft compelling copies that fuels engagement and drives shared success.
           </p>
         </div>
         <div className="service">
           <h3>Editing and Proofreading</h3>
           <p>
-            We meticulously polish our work to perfection, ensuring every detail meets our high standards.
+            We meticulously polish your work to perfection, ensuring every detail meets high standards.
           </p>
         </div>
         <div className="service">
           <h3>Digital Marketing</h3>
           <p>
-            We implement strategic digital marketing efforts that boost our online presence and expand our reach.
+            We implement strategic digital marketing efforts that boost online presence and expand reach.
           </p>
         </div>
         <div className="service">
           <h3>Academic Writing Consultancy</h3>
           <p>
-            We provide expert guidance to enhance our academic projects, ensuring success in our scholarly endeavors.
+            We provide expert guidance to enhance academic projects, ensuring success in scholarly endeavors.
           </p>
         </div>
         <div className="service">
           <h3>Contracts and Agreement Drafting</h3>
           <p>
-            We draft precise and legally sound documents that secure our agreements and protect our interests.
+            We draft precise and legally sound documents that secure agreements and protect interests.
           </p>
         </div>
       </div>
