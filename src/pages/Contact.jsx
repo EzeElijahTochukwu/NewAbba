b7664daa27fd0179242f823c906eb683d86eb1db
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:02:06 +0100

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+// src/pages/Contact.js
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>Get in touch with us for more information.</p>
+  </div>
+);
+
+export default Contact;
