4f7bb5ee400e03596da2e68cdeb55bed563125c4
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:17:12 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,25 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-import App from "./App.jsx"; // This will serve as the Home route
-import Blog from "./pages/Blog";
+import Layout from "./components/Layout"; // Layout with Header and Footer
+import Home from "./pages/Home";
+import Services from "./components/Services";
 import Testimonials from "./pages/Testimonials";
+import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
-import Services from "./components/Services"; // or move to pages if you prefer
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* The Layout route wraps all the pages */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="testimonials" element={<Testimonials />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
       </Routes>
     </Router>
   </React.StrictMode>
