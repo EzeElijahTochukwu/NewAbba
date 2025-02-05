dc7b54627d4825373a58ac2d0f1b4f4de7139314
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-05 02:11:43 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import App from "./App";
-import Blog from "./pages/Blog";
 import "./index.css";
+
+import App from "./App.jsx"; // This will serve as the Home route
+import Blog from "./pages/Blog";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+import Services from "./components/Services"; // or move to pages if you prefer
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/testimonials" element={<Testimonials />} />
         <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   </React.StrictMode>
