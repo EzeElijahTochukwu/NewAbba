e097064fb90afeb492e51d558f8dda94ac3a5564
Elijah Eze Tochukwu <129384793+EzeElijahTochukwu@users.noreply.github.com>
2025-02-07 12:14:21 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <i className="fab fa-linkedin"></i> YesAbba Writes
           </a></p>
           
-          <h3 classname="yes">yesabbawrites</h3>
+          <h3 className="yes">yesabbawrites</h3>
           <div className="social-icons">
             <a href="https://www.instagram.com/yesabbawrites?igsh=MTc5c2d1ZWtwNjB3Zw==" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram"></i>
